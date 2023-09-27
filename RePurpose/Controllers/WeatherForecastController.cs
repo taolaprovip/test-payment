3da2fec5e36b7742ad8963b0cf9f8452c0e37403
taolaprovip <tranthequanganime@gmail.com>
2023-09-27 15:20:44 +0700

    fix-author-weather

--- RePurpose/Controllers/WeatherForecastController.cs
+++ RePurpose/Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ namespace RePurpose.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        [Authorize]
+     
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast

5e169d151e23878e126eab5bfcb3c5a7ff877d06
taolaprovip <tranthequanganime@gmail.com>
2023-09-27 14:03:27 +0700

    fix-swagger

--- RePurpose/Program.cs
+++ RePurpose/Program.cs
@@ -27,10 +27,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-   /* app.UseSwagger();
-    app.UseSwaggerUI();*/
+   
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthentication();
 
 app.UseAuthorization();
